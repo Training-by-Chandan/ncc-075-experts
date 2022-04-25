cc1f6e4743dbd7b301cb9e807edbb8761ec01aad
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-04-25 10:07:04 +0545

    Hello world program added

---
+++ NCC.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NCC.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
