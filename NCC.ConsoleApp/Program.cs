0660f2d78fa12e144ecd9e2e04022f9748c28225
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-04-26 09:39:07 +0545

    data types explained

--- NCC.ConsoleApp/Program.cs
+++ NCC.ConsoleApp/Program.cs
@@ -4,9 +4,47 @@ namespace NCC.ConsoleApp
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            DataTypes();
+            return;
+        }
+
+        static void DataTypes()
+        {
+            int i=10;
+            i=20;
+            
+            //char => int => long => float => double 
+            char c='C';
+            i=c;
+            long l=i;
+            float f=l;
+            double d=f; 
+
+            f=(float)d;
+            l=(long)f;
+            i=(int)l;
+            c=(char)i;
+
+            var x=1;
+            x=10;
+
+            object o="";
+            o=1;
+            o=2f;
+
+            var y=(float)o;
+
+
+            var a="1";
+            var b=2;
+            
+            object o1="";
+            object o2=1;
+
+            var str=i.ToString();
+           
         }
     }
 }
