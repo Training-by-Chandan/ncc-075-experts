637ce92435a855cc8567a255c8f16be3097df47b
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-05-19 10:38:45 +0545

    abstract class explained

---
+++ NCC.ConsoleApp/AbstractClass.cs
@@ -0,0 +1,39 @@
+﻿namespace NCC.ConsoleApp
+{
+    //cannot create the object
+    public abstract class ShapeAbs
+    {
+        public abstract void GetInput();
+
+        protected abstract void Area();
+
+        protected abstract void Perimeter();
+
+        public void Calculate()
+        {
+            Area();
+            Perimeter();
+        }
+    }
+
+    public class SquareAbs : ShapeAbs
+    {
+        private double length;
+
+        public override void GetInput()
+        {
+            Console.WriteLine("Enter the Length of Square");
+            length = Convert.ToDouble(Console.ReadLine());
+        }
+
+        protected override void Area()
+        {
+            Console.WriteLine($"Area of Square = {Math.Pow(length, 2)}");
+        }
+
+        protected override void Perimeter()
+        {
+            Console.WriteLine($"Perimeter of Square = {4 * length}");
+        }
+    }
+}
---
+++ NCC.ConsoleApp/AbstractClassV2.cs
@@ -0,0 +1,66 @@
+﻿namespace NCC.ConsoleApp
+{
+    //cannot create the object
+    public abstract class ShapeAbsV2
+    {
+        protected double area;
+        protected double perimeter;
+
+        protected abstract void getInput();
+
+        protected abstract void calculate();
+
+        public void GetInput()
+        {
+            getInput();
+            calculate();
+        }
+
+        public void Area()
+        {
+            Console.WriteLine($"Area = {area}");
+        }
+
+        public void Perimeter()
+        {
+            Console.WriteLine($"Perimeter = {perimeter}");
+        }
+    }
+
+    public class SquareAbsV2 : ShapeAbsV2
+    {
+        private double length;
+
+        protected override void getInput()
+        {
+            Console.WriteLine("Enter the Length of Square");
+            length = Convert.ToDouble(Console.ReadLine());
+        }
+
+        protected override void calculate()
+        {
+            area = Math.Pow(length, 2);
+            perimeter = 4 * length;
+        }
+    }
+
+    public class RectangleAbsV2 : ShapeAbsV2
+    {
+        private double length;
+        private double breadth;
+
+        protected override void calculate()
+        {
+            area = length * breadth;
+            perimeter = 2 * (length + breadth);
+        }
+
+        protected override void getInput()
+        {
+            Console.WriteLine("Enter the Length of Rectangle");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Breadth of Rectangle");
+            breadth = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
