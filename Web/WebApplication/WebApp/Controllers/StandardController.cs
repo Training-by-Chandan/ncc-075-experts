84af40bc1f11d3050abb7bf8783230de1b1190cf
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-07-21 10:15:55 +0545

    CRUD Operation : Create

---
+++ Web/WebApplication/WebApp/Controllers/StandardController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class StandardController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public StandardController(
+            ApplicationDbContext db
+            )
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var data = db.Standards.ToList();
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Standards model)
+        {
+            if (ModelState.IsValid)
+            {
+                //save to db
+                db.Standards.Add(model);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+    }
+}
