3803b0cf3684ccc06497b04a8402b6fada3fe298
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-07-23 08:19:01 +0545

    New Project added

---
+++ Web/School.System/School.System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace School.System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
