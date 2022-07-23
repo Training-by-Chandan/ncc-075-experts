f138b32b8569444a7d3ba6cb5534565ba894a815
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-07-23 08:18:44 +0545

    edit delete and details page completed

--- Web/WebApplication/WebApp/Controllers/StandardController.cs
+++ Web/WebApplication/WebApp/Controllers/StandardController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using WebApp.Data;
 using WebApp.Models;
 
 namespace WebApp.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class StandardController : Controller
     {
         private readonly ApplicationDbContext db;
@@ -15,6 +17,7 @@ namespace WebApp.Controllers
             this.db = db;
         }
 
+        [AllowAnonymous]
         public IActionResult Index()
         {
             var data = db.Standards.ToList();
@@ -42,6 +45,55 @@ namespace WebApp.Controllers
             {
                 return View(model);
             }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var existing = db.Standards.Find(id);
+            return View(existing);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Standards model)
+        {
+            if (ModelState.IsValid)
+            {
+                //save to db
+                db.Standards.Update(model);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var existing = db.Standards.Find(id);
+            return View(existing);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Standards model)
+        {
+            var existing = db.Standards.Find(model.Id);
+            //save to db
+            db.Standards.Remove(existing);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var existing = db.Standards.Find(id);
+            return View(existing);
         }
     }
 }
